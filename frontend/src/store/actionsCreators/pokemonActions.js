5d9522124438a424f75531d626c99ba9432b1b50
Tikhon <fisioterapevt@gmail.com>
2023-03-09 02:35:23 +0300

    change url api

--- frontend/src/store/actionsCreators/pokemonActions.js
+++ frontend/src/store/actionsCreators/pokemonActions.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-const api = process.env.REACT_APP_API_URL;
-
+//const api = process.env.REACT_APP_API_URL;
+const api = "pokedex-react-api-five.vercel.app/api/v1/";
 export const getPokemonList = createAsyncThunk(
 	"user/getPokemons",
 	async ({ limit, offset }, thunkAPI) => {
